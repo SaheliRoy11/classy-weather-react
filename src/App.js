39c5fc958ab60511f0b748b9108a2d475352459d
Saheli <engr.saheliroy@gmail.com>
2025-07-17 19:42:49 +0530

    Understanding fundamentals of class components in React, before hooks and function components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//React.Component is the parent class.
+// It gives us a couple of methods and one of them is the render() method.Every single react component that is written with classes needs to include this method.This method is equivalent to the function body of a function component.It returns some JSX.
+//In class component if we want to add some state, first call the constructor, it receives props and calls the parent constructor as well by using the super() method.
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    //initialising state
+    //In class components there is a huge state object and not multiple state variables like in functional component
+    this.state = { count: 5 };
+  }
+
+  render() {
+    return (
+      <div>
+        <button>-</button>
+        <span>
+          {
+            //'this' points to the current component instance
+            this.state.count
+          }
+        </span>
+        <button>+</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Counter;
